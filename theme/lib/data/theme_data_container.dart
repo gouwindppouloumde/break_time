8a694ea336f4a86d1be9ae9f001d3921befb6544
G. Hermann Zoma <hermannzoma@gmail.com>
2022-07-13 02:57:00 +0000

    fin de la creation de themedata container

--- theme/lib/data/theme_data_container.dart
+++ theme/lib/data/theme_data_container.dart
@@ -1,3 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:theme/data/color_data.dart';
+import 'package:theme/data/size_data.dart';
+import 'package:theme/data/typography_data.dart';
 
-class ThemeDataContainer {}
+class ThemeDataContainer {
+  const ThemeDataContainer({
+    required this.theme,
+    required this.colors,
+    required this.sizes,
+  });
+
+  factory ThemeDataContainer.main() {
+    final colors = ColorData.main();
+    final theme = ThemeData().fromColors(colors);
+    final sizes = SizeData.main();
+
+    return ThemeDataContainer(theme: theme, colors: colors, sizes: sizes);
+  }
+
+  final ThemeData theme;
+  final ColorData colors;
+  final SizeData sizes;
+}
+
+extension on ThemeData {
+  ThemeData fromColors(ColorData colors) {
+    return ThemeData(
+      textTheme: TypographyData.getMainTextTheme(colors),
+    );
+  }
+}

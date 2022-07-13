6e2651f80d8469217bef896129a2ac1588080fa3
G. Hermann Zoma <hermannzoma@gmail.com>
2022-07-13 17:32:57 +0000

    ajout des exports dans la librairie theme

--- theme/lib/theme.dart
+++ theme/lib/theme.dart
@@ -1,1 +1,8 @@
 library theme;
+
+export 'package:theme/src/data/color_data.dart';
+export 'package:theme/src/data/size_data.dart';
+export 'package:theme/src/data/theme_data_container.dart';
+export 'package:theme/src/data/typography_data.dart';
+
+export 'package:theme/src/widgets/theme_resolver.dart';
